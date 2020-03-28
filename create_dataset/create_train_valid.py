80c15e837e800cc19b550e895f934807f29775f3
delayedkarma <panchajanya.banerjee@gmail.com>
2020-03-28 01:26:58 -0700

    Now also creates directory of non valid and non train files to try out after model fitting

--- create_dataset/create_train_valid.py
+++ create_dataset/create_train_valid.py
@@ -51,7 +51,8 @@ def split_data(root, artist_name, train_split_idx, valid_split_idx):
 
 def check_files(root, artist_name):
     """
-    Function to verify that train and valid files are distinct
+    Function to verify that train and valid files are distinct and to create directories of
+    non train, valid files for each artist for post modeling verification
     :param root: Root directory
     :arg root: str
     :param artist_name: Artist name
@@ -61,16 +62,32 @@ def check_files(root, artist_name):
 
     print(f"Checking for duplicates for {artist_name}")
 
+    src_dir = os.path.join(root, artist_name)
+    test_dir = os.path.join(root, 'files_for_testing', artist_name)
+    
     train_dir = os.path.join(root, 'training', artist_name)
     valid_dir = os.path.join(root, 'validation', artist_name)
 
+    all_files = os.listdir(src_dir)
     train_files = os.listdir(train_dir)
     valid_files = os.listdir(valid_dir)
 
     duplicate_files = len(set(train_files) & set(valid_files))
 
     print(f"{duplicate_files} duplicates for {artist_name}")
-
+    
+    not_train_valid_lst = []
+    
+    for file in all_files:
+        if (file not in train_files) and (file not in valid_files):
+            not_train_valid_lst.append(file)
+            
+    for file in not_train_valid_lst:
+        path_file = os.path.join(src_dir, file)
+        copy2(path_file, test_dir)
+        
+    print(f"Finished creating non train and non valid samples for {artist_name}")       
+            
 
 if __name__ == "__main__":
 
